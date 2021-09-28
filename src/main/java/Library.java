77783204ddda5986245165911f3a91bc80710f04
mitkousa <uk.dimitar@gmail.com>
2021-09-28 15:22:55 +0100

    library hasBookCollection, has capacity tests done

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,5 +4,16 @@ public class Library {
     private ArrayList<Book> booksCollection;
     private Integer capacity;
 
+    public Library(Integer capacity){
+        this.capacity = capacity;
+        this.booksCollection = new ArrayList<>();
+    }
 
+    public int getCapacity() {
+        return this.capacity - this.booksCollection.size();
+    }
+
+    public int getNumberOfBooks() {
+        return this.booksCollection.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
     private Library library;
@@ -6,7 +9,18 @@ public class LibraryTest {
 
     @Before
     public void before(){
+    library = new Library(10);
+    book = new Book("Bible", "Various", "Religious");
+    }
 
+    @Test
+    public void hasCapacity(){
+        assertEquals(10, library.getCapacity());
+    }
+
+    @Test
+    public  void hasBooksCollection(){
+        assertEquals(0, library.getNumberOfBooks());
     }
 
 }
